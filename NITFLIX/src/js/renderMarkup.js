9688fd474ff864435433316d36d8cbf61221b546
Maksym Kozlov <m.kozlov@spd-ukraine.com>
2022-06-11 09:31:32 +0300

    delete folder

--- NITFLIX/src/js/renderMarkup.js
+++
@@ -1,4 +0,0 @@
-const renderMarkup = (data, markup, container) => {
-  container.insertAdjacentHTML('beforeend', markup(data));
-};
-export default renderMarkup;
--- NITFLIX/src/js/services/clearMovieList.js
+++
@@ -1,7 +0,0 @@
-import refs from '../refs';
-
-const clearMovieList = () => {
-  refs.movieContainer.innerHTML = ''; 
-};
-
-export default clearMovieList;
