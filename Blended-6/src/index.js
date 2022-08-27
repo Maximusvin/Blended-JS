5dfff903bf59be1b5bbaf75ed67f84ad82884137
Maksym Kozlov <maximusvin@gmail.com>
2022-08-27 22:53:23 +0300

    Get changes

--- Blended-6/src/index.js
+++ Blended-6/src/index.js
@@ -4,27 +4,37 @@ import { APIService } from './js/services/APIservice';
 import { pokemonDetails } from './js/components/pokemonDetails';
 import './scss/main.scss';
 
-const pokemonApi = new APIService();
+// const pokemonApi = new APIService();
 
-function pokemonMarkup(pokemon) {
-  const data = `<li class="pokemon-card" data-pokemon=${pokemon.name}>    
-        <h2 data-pokemon=${pokemon.name}>${pokemon.name}</h2>
-        <img src=${pokemon.sprites.other.dream_world.front_default} data-pokemon=${pokemon.name} />  
-      </li>`;
+// function pokemonMarkup(pokemon) {
+//   const data = `<li class="pokemon-card" data-pokemon=${pokemon.name}>
+//         <h2 data-pokemon=${pokemon.name}>${pokemon.name}</h2>
+//         <img src=${pokemon.sprites.other.dream_world.front_default} data-pokemon=${pokemon.name} />
+//       </li>`;
 
-  refs.pokemonList.insertAdjacentHTML('beforeend', data);
+//   refs.pokemonList.insertAdjacentHTML('beforeend', data);
+// }
+
+// pokemonApi
+//   .getAllPokemon()
+//   .then(data =>
+//     data.results.forEach(pokemon =>
+//       pokemonApi.getPokemon(pokemon.name).then(pokemonMarkup),
+//     ),
+//   );
+
+// refs.pokemonList.addEventListener('click', openDetailsPokemon);
+
+// function openDetailsPokemon(event) {
+//   pokemonDetails(event.target.dataset.pokemon);
+// }
+
+console.log('werew');
+
+function getFetch() {
+  return fetch('https://randomuser.me/api/').then(res => res.json());
 }
 
-pokemonApi
-  .getAllPokemon()
-  .then(data =>
-    data.results.forEach(pokemon =>
-      pokemonApi.getPokemon(pokemon.name).then(pokemonMarkup),
-    ),
-  );
-
-refs.pokemonList.addEventListener('click', openDetailsPokemon);
-
-function openDetailsPokemon(event) {
-  pokemonDetails(event.target.dataset.pokemon);
-}
+Promise.all([getFetch(), getFetch()])
+  .then(value => console.log(value)) //["promiseA value", "promiseB value"]
+  .catch(error => console.log(error));
